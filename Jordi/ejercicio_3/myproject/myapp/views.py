b9378b3017acf4950fb4bcf2c722ad03859894f3
César <cesarlpb@gmail.com>
2024-07-31 09:51:08 +0200

    Incluyo formateo con indent 4 para que se lea mejor en navegador

--- Jordi/ejercicio_3/myproject/myapp/views.py
+++ Jordi/ejercicio_3/myproject/myapp/views.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from django.http import JsonResponse
 
 def fetch_data(request):
@@ -8,6 +9,10 @@ def fetch_data(request):
     if response.status_code == 200:
         data = response.json()
         formatted_data = [{'id': item['id'], 'title': item['title']} for item in data]
-        return JsonResponse({'data': formatted_data})
+        
+        # Formatear los datos con indentación de 4 espacios
+        formatted_json = json.dumps({'data': formatted_data}, indent=4)
+        
+        return JsonResponse(json.loads(formatted_json), json_dumps_params={'indent': 4})
     else:
         return JsonResponse({'error': 'Unable to fetch data from external API'}, status=500)
