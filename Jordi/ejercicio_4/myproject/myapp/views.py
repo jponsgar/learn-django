237c28e6b3093c54499c251a3c5d1cb1b89a40da
César <cesarlpb@gmail.com>
2024-07-31 09:55:05 +0200

    Añado indent=4 en json

--- Jordi/ejercicio_4/myproject/myapp/views.py
+++ Jordi/ejercicio_4/myproject/myapp/views.py
@@ -1,9 +1,12 @@
+import json
 from django.http import JsonResponse
 from .models import User
 
 def user_list(request):
     users = User.objects.all().values('username', 'email', 'first_name', 'last_name')
     users_list = list(users)
-    return JsonResponse(users_list, safe=False)
-
-
+    
+    # Formatear los datos con indentación de 4 espacios
+    formatted_json = json.dumps(users_list, indent=4)
+    
+    return JsonResponse(json.loads(formatted_json), safe=False, json_dumps_params={'indent': 4})
